07442a60a161e18619b86aee069ee94961c4c11d
Isabell Paulmann <isabell.paulmann@hotmail.de>
2022-07-07 15:18:46 +0200

    add buttons

--- src/pages/DetailPageDarker.js
+++ src/pages/DetailPageDarker.js
@@ -1,5 +1,8 @@
+import {useState} from 'react';
 import {useParams} from 'react-router-dom';
 import styled from 'styled-components';
+
+import './test.css';
 
 import AddStickerButton from '../components/AddStickerButton';
 import DownloadButton from '../components/DownloadButton';
@@ -10,11 +13,21 @@ import StyledFooter from '../styled/StyledFooter';
 export default function DetailPageDarker({wallpapersDark}) {
   const {id} = useParams();
   const thisWallpaper = wallpapersDark.find(wallpaperDark => wallpaperDark.id === Number(id));
+  const [isActive, setActive] = useState(edits);
+  const edits = [];
   return (
     <>
       <Header />
       <StyledBigImageContainer>
-        <StyledImage src={thisWallpaper.image} alt={thisWallpaper.altIMG} />
+        <ButtonGroup>
+          <button onClick={() => setActive(edit[1])}>Brightness+</button>
+          <button onClick={() => setActive(edit)}>Brightness-</button>
+          <button onClick={() => setActive(edit)}>Contrast+</button>
+          <button onClick={() => setActive(edit)}>Contrast-</button>
+          <button onClick={() => setActive(edit)}>Color1</button>
+          <button onClick={() => setActive(edit)}>Color2</button>
+        </ButtonGroup>
+        <StyledImage src={thisWallpaper.image} alt={thisWallpaper.altIMG} className={isActive ? setActive : null} />
       </StyledBigImageContainer>
       <StyledFooter>
         <AddStickerButton />
@@ -28,6 +41,7 @@ export default function DetailPageDarker({wallpapersDark}) {
     </>
   );
 }
+const ButtonGroup = styled.div``;
 
 const StyledImage = styled.img`
   height: 73vh;
