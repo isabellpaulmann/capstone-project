101d4de3fa22a748369594e986b390fedd85502c
Isabell Paulmann <isabell.paulmann@hotmail.de>
2022-07-08 11:29:52 +0200

    add button images

--- src/pages/DetailPageDarker.js
+++ src/pages/DetailPageDarker.js
@@ -5,26 +5,44 @@ import styled from 'styled-components';
 import AddStickerButton from '../components/AddStickerButton';
 import DownloadButton from '../components/DownloadButton';
 import Header from '../components/Header';
+import brightnessminus from '../images/brightnessminus.svg';
+import brightnessplus from '../images/brightnessplus.svg';
+import contrastminus from '../images/contrastminus.svg';
+import contrastplus from '../images/contrastplus.svg';
 import deletesticker from '../images/deletesticker.svg';
+import saturationminus from '../images/saturationminus.svg';
+import saturationplus from '../images/saturationplus.svg';
 import StyledFooter from '../styled/StyledFooter';
 
 export default function DetailPageDarker({wallpapersDark}) {
   const {id} = useParams();
   const thisWallpaper = wallpapersDark.find(wallpaperDark => wallpaperDark.id === Number(id));
-  const imageStyle = {brightness: 0, contrast: 0, saturation: 0};
-  const [isActive, setActive] = useState(imageStyle);
+  const defaultImageStyle = {brightness: 0, contrast: 0, saturation: 0};
+  const [imageStyle, setImageStyle] = useState(defaultImageStyle);
 
   return (
     <>
       <Header />
       <StyledBigImageContainer>
         <ButtonBar>
-          <button onClick={() => setActive({...isActive, brightness: +1})}>Brightness+</button>
-          <button onClick={() => setActive({...isActive, brightness: imageStyle.brightness - 1})}>Brightness-</button>
-          <button onClick={() => setActive({contrast: +1})}>Contrast+</button>
-          <button onClick={() => setActive({...isActive, contrast: isActive.contrast - 1})}>Contrast-</button>
-          <button onClick={() => setActive({...isActive, saturation: isActive.saturation + 1})}>Saturation+</button>
-          <button onClick={() => setActive({...isActive, saturation: isActive.saturation - 1})}>Saturation-</button>
+          <button onClick={() => setImageStyle({...imageStyle, brightness: imageStyle.brightness + 1})}>
+            <img src={brightnessplus} alt="higher brightness" />
+          </button>
+          <button onClick={() => setImageStyle({...imageStyle, brightness: imageStyle.brightness - 1})}>
+            <img src={brightnessminus} alt="lower brightness" />
+          </button>
+          <button onClick={() => setImageStyle({...imageStyle, contrast: imageStyle.contrast + 1})}>
+            <img src={contrastplus} alt="higher contrast" />
+          </button>
+          <button onClick={() => setImageStyle({...imageStyle, contrast: imageStyle.contrast - 1})}>
+            <img src={contrastminus} alt="lower contrast" />
+          </button>
+          <button onClick={() => setImageStyle({...imageStyle, saturation: imageStyle.saturation + 1})}>
+            <img src={saturationplus} alt="higher saturation" />
+          </button>
+          <button onClick={() => setImageStyle({...imageStyle, saturation: imageStyle.saturation - 1})}>
+            <img src={saturationminus} alt="lower saturation" />
+          </button>
         </ButtonBar>
         <StyledImage src={thisWallpaper.image} alt={thisWallpaper.altIMG} style={imageStyle} />
       </StyledBigImageContainer>
@@ -47,6 +65,7 @@ const ButtonBar = styled.div`
 
   button {
     border: none;
+    background-color: transparent;
   }
 `;
 
@@ -82,8 +101,8 @@ const DownloadLink = styled.a`
 `;
 
 const StyledBigImageContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 3fr;
+  display: flex;
+
   justify-content: center;
   align-content: center;
   align-items: center;
