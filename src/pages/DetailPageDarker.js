038b2b90728081547f028ba76fd719740b8dd0b7
Isabell Paulmann <isabell.paulmann@hotmail.de>
2022-07-08 10:16:07 +0200

    add button function

--- src/pages/DetailPageDarker.js
+++ src/pages/DetailPageDarker.js
@@ -11,20 +11,22 @@ import StyledFooter from '../styled/StyledFooter';
 export default function DetailPageDarker({wallpapersDark}) {
   const {id} = useParams();
   const thisWallpaper = wallpapersDark.find(wallpaperDark => wallpaperDark.id === Number(id));
-  const editing = {brightness: 0, contrast: 0, saturation: 0};
-  const [isActive, setActive] = useState(editing);
+  const imageStyle = {brightness: 0, contrast: 0, saturation: 0};
+  const [isActive, setActive] = useState(imageStyle);
 
   return (
     <>
       <Header />
       <StyledBigImageContainer>
-        <button onClick={() => setActive({...isActive, brightness: isActive.brightness + 1})}>Brightness+</button>
-        <button onClick={() => setActive({...isActive, brightness: isActive.brightness - 1})}>Brightness-</button>
-        <button onClick={() => setActive({...isActive, contrast: isActive.contrast + 1})}>Contrast+</button>
-        <button onClick={() => setActive({...isActive, contrast: isActive.contrast - 1})}>Contrast-</button>
-        <button onClick={() => setActive({...isActive, saturation: isActive.saturation + 1})}>Saturation+</button>
-        <button onClick={() => setActive({...isActive, saturation: isActive.saturation - 1})}>Saturation-</button>
-        <StyledImage src={thisWallpaper.image} alt={thisWallpaper.altIMG} className={isActive ? setActive : null} />
+        <ButtonBar>
+          <button onClick={() => setActive({...isActive, brightness: +1})}>Brightness+</button>
+          <button onClick={() => setActive({...isActive, brightness: imageStyle.brightness - 1})}>Brightness-</button>
+          <button onClick={() => setActive({contrast: +1})}>Contrast+</button>
+          <button onClick={() => setActive({...isActive, contrast: isActive.contrast - 1})}>Contrast-</button>
+          <button onClick={() => setActive({...isActive, saturation: isActive.saturation + 1})}>Saturation+</button>
+          <button onClick={() => setActive({...isActive, saturation: isActive.saturation - 1})}>Saturation-</button>
+        </ButtonBar>
+        <StyledImage src={thisWallpaper.image} alt={thisWallpaper.altIMG} style={imageStyle} />
       </StyledBigImageContainer>
       <StyledFooter>
         <AddStickerButton />
@@ -38,6 +40,15 @@ export default function DetailPageDarker({wallpapersDark}) {
     </>
   );
 }
+const ButtonBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  button {
+    border: none;
+  }
+`;
 
 const StyledImage = styled.img`
   height: 73vh;
@@ -72,6 +83,7 @@ const DownloadLink = styled.a`
 
 const StyledBigImageContainer = styled.div`
   display: grid;
+  grid-template-columns: 1fr 3fr;
   justify-content: center;
   align-content: center;
   align-items: center;
