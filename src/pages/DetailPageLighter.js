a211455a1caa77f3a108cbb4ce09a98a75c2e9df
Isabell Paulmann <isabell.paulmann@hotmail.de>
2022-07-08 14:43:03 +0200

    update detailpagelighter

--- src/pages/DetailPageLighter.js
+++ src/pages/DetailPageLighter.js
@@ -1,33 +1,88 @@
+import {useState} from 'react';
 import {useParams} from 'react-router-dom';
 import styled from 'styled-components';
 
 import AddStickerButton from '../components/AddStickerButton';
 import DownloadButton from '../components/DownloadButton';
 import Header from '../components/Header';
+import brightnessminus from '../images/brightnessminus.svg';
+import brightnessplus from '../images/brightnessplus.svg';
+import contrastminus from '../images/contrastminus.svg';
+import contrastplus from '../images/contrastplus.svg';
 import deletesticker from '../images/deletesticker.svg';
+import saturationminus from '../images/saturationminus.svg';
+import saturationplus from '../images/saturationplus.svg';
 import StyledFooter from '../styled/StyledFooter';
 
 export default function DetailPageLighter({wallpapersLight}) {
   const {id} = useParams();
   const thisWallpaper = wallpapersLight.find(wallpaperLight => wallpaperLight.id === Number(id));
+  const defaultImageStyle = {brightness: 1, contrast: 1, saturate: 1};
+  const [imageStyle, setImageStyle] = useState(defaultImageStyle);
+
   return (
     <>
       <Header />
       <StyledBigImageContainer>
-        <StyledImage src={thisWallpaper.image} alt={thisWallpaper.altIMG} />
+        <ButtonBar>
+          <button onClick={() => setImageStyle({...imageStyle, brightness: imageStyle.brightness + 0.1})}>
+            <img src={brightnessplus} alt="higher brightness" />
+          </button>
+          <button onClick={() => setImageStyle({...imageStyle, brightness: imageStyle.brightness - 0.1})}>
+            <img src={brightnessminus} alt="lower brightness" />
+          </button>
+          <button onClick={() => setImageStyle({...imageStyle, contrast: imageStyle.contrast + 0.1})}>
+            <img src={contrastplus} alt="higher contrast" />
+          </button>
+          <button onClick={() => setImageStyle({...imageStyle, contrast: imageStyle.contrast - 0.1})}>
+            <img src={contrastminus} alt="lower contrast" />
+          </button>
+          <button onClick={() => setImageStyle({...imageStyle, saturate: imageStyle.saturate + 0.1})}>
+            <img src={saturationplus} alt="higher saturation" />
+          </button>
+          <button onClick={() => setImageStyle({...imageStyle, saturate: imageStyle.saturate - 0.1})}>
+            <img src={saturationminus} alt="lower saturation" />
+          </button>
+        </ButtonBar>
+        <StyledImage
+          src={thisWallpaper.image}
+          alt={thisWallpaper.altIMG}
+          style={{
+            filter: `brightness(${imageStyle.brightness}) contrast(${imageStyle.contrast}) saturate(${imageStyle.saturate})`,
+          }}
+        />
+        <StyledPlaceholder />
       </StyledBigImageContainer>
       <StyledFooter>
         <AddStickerButton />
         <DownloadLink href={thisWallpaper.image} download={thisWallpaper.image}>
-          <StyledColorButton>
+          <StyledColorButtonContainer>
             <DownloadButton />
-          </StyledColorButton>
+          </StyledColorButtonContainer>
         </DownloadLink>
         <img src={deletesticker} alt="delete sticker" />
       </StyledFooter>
     </>
   );
 }
+
+const StyledPlaceholder = styled.div`
+  background-color: transparent;
+`;
+
+const ButtonBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  button {
+    border: none;
+    background-color: transparent;
+    padding: 0;
+    margin-left: -3px;
+    margin-right: -3px;
+  }
+`;
 
 const StyledImage = styled.img`
   height: 73vh;
@@ -44,7 +99,7 @@ const StyledImage = styled.img`
   }
 `;
 
-const StyledColorButton = styled.div`
+const StyledColorButtonContainer = styled.div`
   button {
     background-color: #cd8282;
     img {
@@ -62,6 +117,7 @@ const DownloadLink = styled.a`
 
 const StyledBigImageContainer = styled.div`
   display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
   justify-content: center;
   align-content: center;
   align-items: center;
