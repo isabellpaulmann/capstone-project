6848bf9032c0ca53f2731ece846fe7f0446fae25
Isabell Paulmann <isabell.paulmann@hotmail.de>
2022-07-08 09:50:28 +0200

    add button function

--- src/pages/DetailPageDarker.js
+++ src/pages/DetailPageDarker.js
@@ -2,8 +2,6 @@ import {useState} from 'react';
 import {useParams} from 'react-router-dom';
 import styled from 'styled-components';
 
-import './test.css';
-
 import AddStickerButton from '../components/AddStickerButton';
 import DownloadButton from '../components/DownloadButton';
 import Header from '../components/Header';
@@ -13,46 +11,20 @@ import StyledFooter from '../styled/StyledFooter';
 export default function DetailPageDarker({wallpapersDark}) {
   const {id} = useParams();
   const thisWallpaper = wallpapersDark.find(wallpaperDark => wallpaperDark.id === Number(id));
-  const [isActive, setActive] = useState(false);
-  const [isActive2, setActive2] = useState(false);
-  const [isActive3, setActive3] = useState(false);
-  const [isActive4, setActive4] = useState(false);
-  const [isActive5, setActive5] = useState(false);
-  const [isActive6, setActive6] = useState(false);
+  const editing = {brightness: 0, contrast: 0, saturation: 0};
+  const [isActive, setActive] = useState(editing);
 
-  const higherBrightness = () => {
-    setActive(!isActive);
-  };
-  const lowerBrightness = () => {
-    setActive2(!isActive2);
-  };
-  const higherContrast = () => {
-    setActive3(!isActive3);
-  };
-  const lowerContrast = () => {
-    setActive4(!isActive4);
-  };
-  const changeColor1 = () => {
-    setActive5(!isActive5);
-  };
-  const changeColor2 = () => {
-    setActive6(!isActive6);
-  };
   return (
     <>
       <Header />
       <StyledBigImageContainer>
-        <button onClick={higherBrightness}>Brightness+</button>
-        <button onClick={lowerBrightness}>Brightness-</button>
-        <button onClick={higherContrast}>Contrast+</button>
-        <button onClick={lowerContrast}>Contrast-</button>
-        <button onClick={changeColor1}>Color1</button>
-        <button onClick={changeColor2}>Color2</button>
-        <StyledImage
-          src={thisWallpaper.image}
-          alt={thisWallpaper.altIMG}
-          className={isActive ? 'makeBrighter' : null}
-        />
+        <button onClick={() => setActive({...isActive, brightness: isActive.brightness + 1})}>Brightness+</button>
+        <button onClick={() => setActive({...isActive, brightness: isActive.brightness - 1})}>Brightness-</button>
+        <button onClick={() => setActive({...isActive, contrast: isActive.contrast + 1})}>Contrast+</button>
+        <button onClick={() => setActive({...isActive, contrast: isActive.contrast - 1})}>Contrast-</button>
+        <button onClick={() => setActive({...isActive, saturation: isActive.saturation + 1})}>Saturation+</button>
+        <button onClick={() => setActive({...isActive, saturation: isActive.saturation - 1})}>Saturation-</button>
+        <StyledImage src={thisWallpaper.image} alt={thisWallpaper.altIMG} className={isActive ? setActive : null} />
       </StyledBigImageContainer>
       <StyledFooter>
         <AddStickerButton />
