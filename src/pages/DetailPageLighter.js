717577b1223fc1d9e1daa663d4cf264fc409ddfe
Isabell Paulmann <isabell.paulmann@hotmail.de>
2022-07-25 09:19:44 +0200

    fix media queries

--- src/pages/DetailPageLighter.js
+++ src/pages/DetailPageLighter.js
@@ -163,22 +163,22 @@ const StyledBigImageContainer = styled.div`
   animation: ${fadeIn} 2s;
 
   @media (min-width: 390px) {
-    margin-top: 60px;
+    margin-top: 6vh;
   }
   @media (min-width: 411px) {
     margin-top: 1px;
   }
   @media (min-width: 412px) {
-    margin-top: 60px;
+    margin-top: 6vh;
   }
   @media (min-width: 414px) {
-    margin-top: 60px;
+    margin-top: 6vh;
   }
   @media (min-width: 540px) {
     margin-top: 5px;
   }
   @media (min-width: 768px) {
-    margin-top: 20px;
+    margin-top: 3vh;
   }
   @media (min-width: 820px) {
     margin-top: 10px;
